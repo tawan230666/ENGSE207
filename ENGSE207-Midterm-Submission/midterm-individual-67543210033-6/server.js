1d4fd1b82fa73ea11a2dfb351bcd0ef10737c4e4
tawan230666 <tawantippkun200501@gmail.com>
2026-01-12 12:10:47 +0700

    Final Submission: Ready to send

--- ENGSE207-Midterm-Submission/midterm-individual-67543210033-6/server.js
+++ ENGSE207-Midterm-Submission/midterm-individual-67543210033-6/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const bookRoutes = require('./src/presentation/routes/bookRoutes');
 const errorHandler = require('./src/presentation/middlewares/errorHandler');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
